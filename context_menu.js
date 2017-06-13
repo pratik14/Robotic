4a7499f32e542c135d87f7eaf5cf661d4d00cb8c
pratik14 <pratik14shah@gmail.com>
2017-06-13 19:28:41 +0530

    Keep just hover and page contain option in context menu

--- context_menu.js
+++ context_menu.js
@@ -7,38 +7,23 @@ var ContextMenu = {
       "title": "Robotic"
     };
 
-    var allOpt = [];
-
     chrome.contextMenus.create(menuItem);
 
-    Object.keys(contextMenuOptions).forEach(function(key,index) {
+    for (var key in contextMenuOptions) {
       chrome.contextMenus.create({
         id: key,
-        title: key,
+        title: contextMenuOptions[key],
         parentId: 'Robotic'
       });
-      for(var propt in contextMenuOptions[key]){
-        var subMenu = contextMenuOptions[key][propt];
-        chrome.contextMenus.create({
-          id: subMenu,
-          title: subMenu,
-          parentId: key
-        });
-      }
-    });
-
-
+    };
 
     chrome.contextMenus.onClicked.addListener(function(clickData){
-      // var roboticOpt = allOpt.indexOf(clickData.menuItemId) != -1;
-      var roboticOpt = true;
-      if (roboticOpt){
-        var content = host.tabs;
-        content.query({active: true}, (tabs) => {
-          var opt_name = clickData.menuItemId;
-          content.sendMessage(tabs[0].id, { operation: "contextMenuClick", opt_name: opt_name });
-        });
-      }
+      var content = host.tabs;
+
+      content.query({active: true}, (tabs) => {
+        var opt_name = clickData.menuItemId;
+        content.sendMessage(tabs[0].id, { operation: "contextMenuClick", opt_name: opt_name });
+      });
     });
   },
 
